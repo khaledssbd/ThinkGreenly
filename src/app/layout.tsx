4c3f23303e9fde5aa9da731338a630f0a03ed8b7
khaledssbd <khaledssbd@gmail.com>
2025-05-02 20:54:04 +0600

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Providers from "@/providers/Providers";
+import Providers from "@/Providers/Providers";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
--- src/components/Basics/NavBar.tsx
+++ src/components/Basics/NavBar.tsx
@@ -1,85 +1,101 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
-
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useTheme } from 'next-themes';
+import { ModeToggle } from '../ModeToggle';
+import { useUser } from '@/context/UserContext';
 // import { MdEnergySavingsLeaf } from "react-icons/md";
-import logo from "../../assets/logo.png";
-import { ModeToggle } from "../ModeToggle";
+
 const NavBar = () => {
   const pathname = usePathname();
-  //.log(pathname);
-  const links = (
-    <>
-      <Link
-        href="/"
-        className={
-          pathname === "/" ? "border-b-2 border-green-300" : "border-b-0"
-        }
-      >
-        <h1>Home</h1>
-      </Link>
-      <Link
-        href="/ideas"
-        className={
-          pathname === "/ideas" ? "border-b-2 border-green-300" : "border-b-0"
-        }
-      >
-        <h1>Ideas</h1>
-      </Link>
-      <Link
-        href="/blogs"
-        className={
-          pathname === "/blog" ? "border-b-2 border-green-300" : "border-b-0"
-        }
-      >
-        <h1>Blogs</h1>
-      </Link>
-      <Link
-        href="/about-us"
-        className={
-          pathname === "/aboutus" ? "border-b-2 border-green-300" : "border-b-0"
-        }
-      >
-        <h1>About Us</h1>
-      </Link>
-    </>
-  );
+  const { theme } = useTheme();
+  const { user } = useUser();
+
+  const pathLinks = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'Ideas',
+      path: '/ideas',
+    },
+    {
+      name: 'Blogs',
+      path: '/blogs',
+    },
+    {
+      name: 'About Us',
+      path: '/about-us',
+    },
+    {
+      name: user ? 'Dashboard' : '',
+      path: user ? `/dashboard/${user?.role}` : '/login',
+    },
+  ];
 
   return (
-    <div className="flex  items-center w-[95%] mt-6 mx-auto justify-between">
-      <div className="font-bold ml-5  text-2xl flex gap-2 items-center">
-        <Image src={logo} width={135} height={10} alt="logo" />
-      </div>
-      <div className="flex gap-8 ml-30">{links}</div>
-      <div>
+    <div className=''>
+      <div className="flex items-center w-[95%] mt-6 mx-auto justify-between">
+        <div className="font-bold ml-5 text-2xl flex gap-2 items-center">
+          <Image
+            src={theme === 'light' ? '/dark-logo.png' : '/logo.png'}
+            width={135}
+            height={10}
+            alt="logo"
+          />
+        </div>
+
+        <div className="flex gap-8 ml-30">
+          {pathLinks.map(({ name, path }) => (
+            <Link
+              key={name}
+              href={path}
+              className={
+                pathname === path ? 'border-b-2 border-green-300' : 'border-b-0'
+              }
+            >
+              <h4>{name}</h4>
+            </Link>
+          ))}
+        </div>
+
+        {/* <div>
         <button
           className={
-            pathname === "/dashboard"
-              ? "border-b-2 border-dashed border-2 border-green-300 px-4 py-2"
-              : "border-4 border-green-300 px-2 py-1 border-dotted rounded-3xl"
+            pathname === '/dashboard'
+              ? 'border-b-2 border-dashed border-2 border-green-300 px-4 py-2'
+              : 'border-4 border-green-300 px-2 py-1 border-dotted rounded-3xl'
           }
         >
           <Link href="/dashboard">
             <h1>Dashboard</h1>
           </Link>
         </button>
-      </div>
-      <div className="">
-        <Link href="/login">
-          <button className="bg-green-500  rounded-lg px-2 py-1">Login</button>
-        </Link>
-        {/* </> */}
-        {/* )} */}
-        <Link href="register">
-          {" "}
-          <button className="bg-green-950 ml-6  rounded-lg px-2 py-1">
+      </div> */}
+        <div className="space-x-3">
+          <ModeToggle />
+          {user ? (
+            <Link href="/login">
+              <button className="bg-green-500 rounded-lg px-2 py-1">
+                Login
+              </button>
+            </Link>
+          ) : (
+            <Link href="/login">
+              <button className="bg-green-500 rounded-lg px-2 py-1">
+                Login
+              </button>
+            </Link>
+          )}
+          {/* <Link href="/register">
+          <button className="bg-green-950 ml-6 rounded-lg px-2 py-1">
             Register
           </button>
-        </Link>
-        <ModeToggle />
+        </Link> */}
+        </div>
       </div>
     </div>
   );
