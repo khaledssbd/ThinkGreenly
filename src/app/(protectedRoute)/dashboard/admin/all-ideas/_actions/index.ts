c4c5cf43e7a0dd38e7154681a178160c5b0d9871
khaledssbd <khaledssbd@gmail.com>
2025-05-02 23:41:19 +0600

    ss

---
+++ src/app/(protectedRoute)/dashboard/admin/all-ideas/_actions/index.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const getAllIdeas = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/admin/ideas`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch rental houses");
+    }
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
