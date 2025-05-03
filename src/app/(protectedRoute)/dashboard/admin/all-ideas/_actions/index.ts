e1f8e46febe083588ad00fa526f79141e5a9de7d
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-05-03 20:26:46 +0600

    feat(user): update user status fixed: sidebar path,some design

--- src/app/(protectedRoute)/dashboard/admin/all-ideas/_actions/index.ts
+++ src/app/(protectedRoute)/dashboard/admin/all-ideas/_actions/index.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const getAllIdeas = async () => {
   try {
+
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/admin/ideas`);
 
     if (!res.ok) {
-      throw new Error("Failed to fetch rental houses");
+      throw new Error("Failed to fetch ideas");
     }
     const data = await res.json();
-    console.log(data);
+    console.log(data,"all data only:");
     return data;
   } catch (error: any) {
     throw new Error(error.message);
