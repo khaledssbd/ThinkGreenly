a178d08d785cfaa4ddddde4bdbcacad68a013125
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-05-03 23:22:58 +0600

    feature(user): admin can updated user status like active deactive feature(idea): admin can approve or reject idea refactor data table

--- src/app/(protectedRoute)/dashboard/admin/all-ideas/_components/AllIdeasModule.tsx
+++ src/app/(protectedRoute)/dashboard/admin/all-ideas/_components/AllIdeasModule.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { getAllIdeas } from "../_actions";
 import IdeasTable from "./IdeasTable";
-import { dummyIdeas } from "../_data";
+// import { dummyIdeas } from "../_data";
 
 const AllIdeasModule = async () => {
   const ideas = await getAllIdeas();
   console.log(ideas, "all ideas:");
 
-  const data = ideas?.data?.length < 0 ? ideas?.data : dummyIdeas;
+  // const data = ideas?.data?.length < 0 ? ideas?.data : dummyIdeas;
   return (
     <div>
       <IdeasTable data={ideas?.data} />
