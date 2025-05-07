8f68cea006dbd5cd1ab4549198cf997eae4ace88
khaledssbd <khaledssbd@gmail.com>
2025-05-07 19:31:12 +0600

    [REFACTOR: EditIdeaForm]

--- src/app/(protectedRoute)/member/create-idea/_components/IdeaValidation.ts
+++ src/app/(protectedRoute)/member/create-idea/_components/IdeaValidation.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 
 export const ideaCreationSchema = z.object({
+  id: z
+    .string({
+      required_error: '',
+    })
+    .optional(),
+
   title: z
     .string({ required_error: 'Title is required!' })
     .trim()
@@ -32,35 +38,44 @@ export const ideaCreationSchema = z.object({
 });
 
 export const ideaDraftSchema = z.object({
+  id: z
+    .string({
+      required_error: '',
+    })
+    .optional(),
+
   title: z
-    .string({ required_error: "Title is required!" })
+    .string({ required_error: 'Title is required!' })
     .trim()
-    .min(10, { message: "Title must have minimum 10 characters!" })
+    .min(10, { message: 'Title must have minimum 10 characters!' })
     .max(60, { message: "Title can't exceed 60 characters!" }),
 
   problemStatement: z
-    .string({ required_error: "Problem Statement is required!" })
+    .string({ required_error: 'Problem Statement is required!' })
     .trim()
-    .min(15, "Problem statement must be at least 15 characters!").optional(),
+    .min(15, 'Problem statement must be at least 15 characters!')
+    .optional(),
 
   solution: z
-    .string({ required_error: "Solution is required!" })
+    .string({ required_error: 'Solution is required!' })
     .trim()
-    .min(20, "Solution must be at least 20 characters long!").optional(),
+    .min(20, 'Solution must be at least 20 characters long!')
+    .optional(),
 
   description: z
-    .string({ required_error: "Description Statement is required!" })
+    .string({ required_error: 'Description Statement is required!' })
     .trim()
-    .min(20, "Description must be 20 characters long!").optional(),
+    .min(20, 'Description must be 20 characters long!')
+    .optional(),
 
   price: z
-    .number({ required_error: "Price is required!" })
-    .nonnegative("Price is must a positive value!").optional(),
+    .number({ required_error: 'Price is required!' })
+    .nonnegative('Price is must a positive value!')
+    .optional(),
 
-  categoryId: z.string({
-    required_error: "Must select a category!",
-  }).optional(),
-  id: z.string({
-    required_error: "",
-  }).optional(),
+  categoryId: z
+    .string({
+      required_error: 'Must select a category!',
+    })
+    .optional(),
 });
